6c92a1f24f6255f57805eb1c8c9fef6709ab47d7
Renan Garcia <renan.garcia@fretebras.com.br>
2019-06-16 19:21:03 -0300

    Change author to owner

--- resolvers/Commit.js
+++ resolvers/Commit.js
@@ -2,6 +2,6 @@ module.exports = {
     commit_msg: (commit) => commit.commit.message,
     author_name: (commit) => commit.commit.author.name,
     author_email: (commit) => commit.commit.author.email,
-    author_avatar_url: (commit) => commit.author.avatar_url,
+    author_avatar_url: (commit) => commit.owner.avatar_url,
     author_created_at: (commit) => commit.commit.author.date
 }
