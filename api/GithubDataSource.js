1a6629cf05ec657225080ad050d24dae4a66b170
Renan Garcia <renan.garcia@fretebras.com.br>
2019-06-16 19:12:31 -0300

    Add return res on api github

--- api/GithubDataSource.js
+++ api/GithubDataSource.js
@@ -13,14 +13,16 @@ class GithubDataSource extends RESTDataSource {
   async getUsers() {
     const users = await this.get(`/users`).then(res => {
       if (typeof res === 'string') return JSON.parse(res)
+      return res
     });
-    
+
     return users ? (users.length >= 3 ? users.slice(0, 3) : users) : [];
   }
 
   async getUser(login) {
     const user = await this.get(`/users/${login}`).then(res => {
       if (typeof res === 'string') return JSON.parse(res)
+      return res
     });
 
     return user;
@@ -29,6 +31,7 @@ class GithubDataSource extends RESTDataSource {
   async getRepos(login) {
     const repos = await this.get(`/users/${login}/repos`).then(res => {
       if (typeof res === 'string') return JSON.parse(res)
+      return res
     });
 
     return repos ? (repos.length >= 3 ? repos.slice(0, 3) : repos) : [];
@@ -37,6 +40,7 @@ class GithubDataSource extends RESTDataSource {
   async getCommits(login, repoName) {
     const commits = await this.get(`/repos/${login}/${repoName}/commits`).then(res => {
       if (typeof res === 'string') return JSON.parse(res)
+      return res
     });
 
     return commits ? (commits.length >= 3 ? commits.slice(0, 3) : commits) : [];
